43ae8ef6b95871548cc6a89f608ddef636924bbe
ksadowskiwsb <krzysztofsadowski1989@gmail.com>
2021-10-02 19:34:21 +0200

    cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,37 @@ public class Main {
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Podaj wiek: ");
+        int age = scan.nextInt();
+
+        if (age % 3 == 0) {
+            System.out.println("Wiek jest podzielny przez 3.");
+        } else {
+            System.out.println("Wiek nie jest podzielny przez 3.");
+        }
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+        System.out.println("Podaj nr indeksu: ");
+        int indexNumber = scan.nextInt();
 
+        String result = (indexNumber % 2 == 0) ? "parzysty" : "nieparzysty";
+        System.out.println("Numer indeksu jest " + result);
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        System.out.println("Podaj liczbę zmiennoprzecinkową x: ");
+        double x = scan.nextDouble();
 
+        if (x < 0) {
+            System.out.println("x < 0");
+        } else if (x < 10) {
+            System.out.println("x < 10");
+        } else if ( x < 20) {
+            System.out.println("x < 20");
+        } else {
+            System.out.println("x >= 20");
+        }
     }
 }
