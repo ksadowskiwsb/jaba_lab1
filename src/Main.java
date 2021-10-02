f6b1f1738db43a669fa104640f2696e671d493d6
ksadowskiwsb <krzysztofsadowski1989@gmail.com>
2021-10-02 19:49:02 +0200

    cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,22 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        Scanner scan = new Scanner(System.in);
+        double[] arr = new double[5];
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Podaj %s liczbę: ", i + 1);
+            arr[i] = scan.nextDouble();
+        }
+
+        System.out.print("arr = [");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.print(arr[i] + "]");
+            }
+        }
     }
 }
