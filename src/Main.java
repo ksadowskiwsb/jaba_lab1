73d3aaf066464260177d720ffb8f43e8929c0784
ksadowskiwsb <krzysztofsadowski1989@gmail.com>
2021-10-02 18:56:41 +0200

    cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("This is some print.");
+        System.out.println("And this is another one.");
+        System.out.println("This is " + " " + "print  " + "with string concatenation!");
 
+        System.out.printf("This is %s print in %d exercise.\n", "the coolest", 1);
+
+        System.out.print("This is print");
+        System.out.print(" without");
+        System.out.print(" new line.");
 
     }
 }
