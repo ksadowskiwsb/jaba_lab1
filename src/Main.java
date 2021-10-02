af342193b0adad8c1f60cb678b26d3cacc59f75a
ksadowskiwsb <krzysztofsadowski1989@gmail.com>
2021-10-02 19:22:54 +0200

    cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        boolean a = true;
+        boolean b = false;
+        int x = 10;
+        int y = 20;
 
+        System.out.printf("((a && !b) || (x > y)) && (x * y > y): %s\n", ((a && !b) || (x > y)) && (x * y > y));
+        System.out.printf("((!a && b) || (x >= y)) && (a || !b): %s\n", ((!a && b) || (x >= y)) && (a || !b));
+        System.out.printf("(x < y) && ((x >= y) || (x + y <= x)): %s\n", (x < y) && ((x >= y) || (x + y <= x)));
     }
 }
